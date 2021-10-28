5bbb061cd1d7bb9c3b09b7d16988e229becdc152
Declan Edgecombe <edgecdec@gmail.com>
2021-10-27 23:40:36 -0700

    Refactored see if anyone finished file.

--- gen_scav_mura.py
+++ gen_scav_mura.py
@@ -138,10 +138,20 @@ def pack():
         packf.write(packLines)
     packf.close()
 
-    text = '# Check if anyone has finished\nexecute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] run say HAS COMPLETED THE SCAVENGER HUNT!\nexecute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!FINISHED] run summon firework_rocket ~ ~3 ~ {LifeTime:20,FireworksItem:{id:firework_rocket,Count:1,tag:{Fireworks:{Flight:2,Explosions:[{Type:1,Flicker:0,Trail:1,Colors:[I;8073150,14602026],FadeColors:[I;14602026]}]}}}}\ntag @a[distance=..5,scores={TreasuresLeft=..0},tag=!FINISHED] add FINISHED\nexecute as @a[tag=FINISHED, scores={TreasuresLeft=..0}] run scoreboard players add @a[tag=FINISHED, scores={TreasuresLeft=..0}] ScavengerRanking 1'
-    with open(DIR_PATH + '/items/see_if_anyone_finished.mcfunction', 'w+') as seef:
-        seef.write(text)
+    checkIfFinishCommand = '# Check if anyone has just finished\n'
+    checkIfFinishCommand += 'execute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] run say HAS COMPLETED THE SCAVENGER HUNT!\n\n'
 
+    oneTimeCelebrationCommand = '# If anyone has just finished, congratulate them\n'
+    oneTimeCelebrationCommand += 'execute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] run summon firework_rocket ~ ~3 ~ {LifeTime:20,FireworksItem:{id:firework_rocket,Count:1,tag:{Fireworks:{Flight:2,Explosions:[{Type:1,Flicker:0,Trail:1,Colors:[I;8073150,14602026],FadeColors:[I;14602026]}]}}}}\n\n'
+
+    addFinishedTagCommand = '# Add the finished tag to anyone who has finished to prevent multiple celebrations\n'
+    addFinishedTagCommand += 'tag @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] add finished\n\n'
+
+    increaseRankOfFinishedPlayersCommand = '# Add 1 to rank of all players who have finished\n'
+    increaseRankOfFinishedPlayersCommand += 'execute as @a[tag=finished, scores={TreasuresLeft=..0}] run scoreboard players add @s ScavengerRanking 1\n\n'
+
+    with open(f'{DIR_PATH}items/see_if_anyone_finished.mcfunction', 'w+') as seef:
+        seef.write(f'{checkIfFinishCommand}{oneTimeCelebrationCommand}{addFinishedTagCommand}{increaseRankOfFinishedPlayersCommand}')
 
 generate()
 pack()
--- scavenger_mura/data/scavenger/functions/items/see_if_anyone_finished.mcfunction
+++ scavenger_mura/data/scavenger/functions/items/see_if_anyone_finished.mcfunction
@@ -1,5 +1,12 @@
-# Check if anyone has finished
+# Check if anyone has just finished
 execute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] run say HAS COMPLETED THE SCAVENGER HUNT!
-execute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!FINISHED] run summon firework_rocket ~ ~3 ~ {LifeTime:20,FireworksItem:{id:firework_rocket,Count:1,tag:{Fireworks:{Flight:2,Explosions:[{Type:1,Flicker:0,Trail:1,Colors:[I;8073150,14602026],FadeColors:[I;14602026]}]}}}}
-tag @a[distance=..5,scores={TreasuresLeft=..0},tag=!FINISHED] add FINISHED
-execute as @a[tag=FINISHED, scores={TreasuresLeft=..0}] run scoreboard players add @a[tag=FINISHED, scores={TreasuresLeft=..0}] ScavengerRanking 1
+
+# If anyone has just finished, congratulate them
+execute as @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] run summon firework_rocket ~ ~3 ~ {LifeTime:20,FireworksItem:{id:firework_rocket,Count:1,tag:{Fireworks:{Flight:2,Explosions:[{Type:1,Flicker:0,Trail:1,Colors:[I;8073150,14602026],FadeColors:[I;14602026]}]}}}}
+
+# Add the finished tag to anyone who has finished to prevent multiple celebrations
+tag @a[distance=..5,scores={TreasuresLeft=..0},tag=!finished] add finished
+
+# Add 1 to rank of all players who have finished
+execute as @a[tag=finished, scores={TreasuresLeft=..0}] run scoreboard players add @s ScavengerRanking 1
+
