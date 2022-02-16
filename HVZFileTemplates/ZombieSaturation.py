d0a99aac95f028f8260d08b265572e012bb6bf4d
Declan Edgecombe <edgecdec@gmail.com>
2022-02-15 23:21:22 -0800

    Fixed zombie saturation command

--- HVZFileTemplates/ZombieSaturation.py
+++ HVZFileTemplates/ZombieSaturation.py
@@ -2,8 +2,8 @@ from Util import *
 from HVZConstants import *
 
 zombieSatStr = f"execute as @p run effect give @a[team={ZOMBIE_TEAM_NAME}] minecraft:saturation 1 0 true\n"
-scoreboardAddStr = f"scoreboard players add {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} 1\n"
-helperActivateStr = f"execute if score {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} matches {ZOMBIE_SATURATION_TICKS_BETWEEN} run function hvz:control/zombie_timer\n"
+scoreboardAddStr = f"execute as @p run scoreboard players add {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} 1\n"
+helperActivateStr = f"execute if score {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} matches {ZOMBIE_SATURATION_TICKS_BETWEEN} run function hvz:control/zombie_saturation\n"
 scoreboardResetStr = f"execute if score {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} matches {ZOMBIE_SATURATION_TICKS_BETWEEN} run scoreboard players set {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} 0\n"
 
 def generateZombieSaturationFile():
--- hvz/data/hvz/functions/control/zombie_timer.mcfunction
+++ hvz/data/hvz/functions/control/zombie_timer.mcfunction
@@ -1,3 +1,3 @@
-scoreboard players add zombieSatTimer ZombieSatTimer 1
-execute if score zombieSatTimer ZombieSatTimer matches 6 run function hvz:control/zombie_timer
+execute as @p run scoreboard players add zombieSatTimer ZombieSatTimer 1
+execute if score zombieSatTimer ZombieSatTimer matches 6 run function hvz:control/zombie_saturation
 execute if score zombieSatTimer ZombieSatTimer matches 6 run scoreboard players set zombieSatTimer ZombieSatTimer 0
