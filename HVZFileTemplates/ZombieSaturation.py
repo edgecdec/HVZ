77444098729b8f37efd8fc627239eb7448d40da8
Declan Edgecombe <edgecdec@gmail.com>
2022-02-26 14:34:14 -0800

    Fixes bug with wrong code for zombie sat

--- HVZFileTemplates/ZombieSaturation.py
+++ HVZFileTemplates/ZombieSaturation.py
@@ -2,6 +2,8 @@ from Util import *
 from HVZConstants import *
 
 zombieSatStr = f"execute as @p run effect give @a[team={ZOMBIE_TEAM_NAME}] minecraft:saturation 1 0 true\n"
+addMobsToTeamsStr = "function hvz:control/add_mobs_to_teams\n"
+updateKillsStr = "function hvz:control/kill_update_helper\n"
 scoreboardAddStr = f"execute as @p run scoreboard players add {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} 1\n"
 helperActivateStr = f"execute if score {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} matches {ZOMBIE_SATURATION_TICKS_BETWEEN} run function hvz:control/zombie_saturation\n"
 scoreboardResetStr = f"execute if score {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} matches {ZOMBIE_SATURATION_TICKS_BETWEEN} run scoreboard players set {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE_SATURATION_TIMER_SCOREBOARD} 0\n"
@@ -9,6 +11,8 @@ scoreboardResetStr = f"execute if score {ZOMBIE_SATURATION_TIMER_PLAYER} {ZOMBIE
 def generateZombieSaturationFile():
     with open(f"{CONTROL_FILE_PATH}zombie_saturation.mcfunction", 'w') as outfile:
         outfile.write(zombieSatStr)
+        outfile.write(addMobsToTeamsStr)
+        outfile.write(updateKillsStr)
 
     with open(f"{CONTROL_FILE_PATH}zombie_timer.mcfunction", 'w') as outfile:
         outfile.write(scoreboardAddStr)
